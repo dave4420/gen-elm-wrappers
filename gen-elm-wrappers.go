5d8331068f4cf358003bfa73aad218650047490e
Dave Hinton <dave@4420.uk>
2023-10-07 19:20:32 +0100

    add noddy dictModule

--- gen-elm-wrappers.go
+++ gen-elm-wrappers.go
@@ -2,6 +2,57 @@ package main
 
 import "fmt"
 
+type module interface {
+	name() string
+	source() []string
+}
+
+type dictModule struct {
+	moduleName  string
+	typeName    string
+	keyTypeName string
+}
+
+func (module dictModule) name() string {
+	return module.moduleName
+}
+
+func (module dictModule) source() []string {
+	return []string{
+		"module " + module.moduleName + " exposing (..)",
+	}
+}
+
+func writeModule(path string, module module) {
+	fmt.Println("Writing module", module.name(), "to", path)
+	for _, line := range module.source() {
+		fmt.Println(line)
+	}
+}
+
+type config struct {
+	path    string
+	modules []module
+}
+
+func readConfig() config {
+	conf := config{
+		path: "src",
+		modules: []module{
+			dictModule{
+				moduleName:  "Data.ByDate",
+				typeName:    "ByDate",
+				keyTypeName: "Date",
+			},
+		},
+	}
+	return conf
+}
+
 func main() {
-	fmt.Println("Go? Really?")
+	var conf = readConfig()
+
+	for _, module := range conf.modules {
+		writeModule(conf.path, module)
+	}
 }
