851fd12cec5154f3a770a61ed9e3befbd99f21d9
Dave Hinton <dave@4420.uk>
2023-10-23 16:48:26 +0100

    make defs be methods on dictModule

--- dict-module.go
+++ dict-module.go
@@ -6,19 +6,19 @@ func (module dictModule) name() string {
 	return module.wrapperType.moduleName
 }
 
-func dictDef(wrapperType identifier, privateKeyId identifier) definition {
+func (module dictModule) dictDef() definition {
 	return definition{
-		localName: wrapperType.name,
+		localName: module.wrapperType.name,
 		source: []string{
 			strings.Join(
 				[]string{
 					"type",
-					wrapperType.name,
+					module.wrapperType.name,
 					"a",
 					"=",
-					wrapperType.name,
+					module.wrapperType.name,
 					"(Dict",
-					privateKeyId.fullName(),
+					module.privateKeyType.fullName(),
 					"a)",
 				},
 				" ",
@@ -27,31 +27,31 @@ func dictDef(wrapperType identifier, privateKeyId identifier) definition {
 	}
 }
 
-func emptyDictDef(wrapperType identifier, publicKeyId identifier) definition {
+func (module dictModule) emptyDictDef() definition {
 	return definition{
 		localName: "empty",
 		source: []string{
-			"empty : " + wrapperType.name + " a",
-			"empty = " + wrapperType.name + " Dict.empty",
+			"empty : " + module.wrapperType.name + " a",
+			"empty = " + module.wrapperType.name + " Dict.empty",
 		},
 	}
 }
 
-func singletonDictDef(wrapperType identifier, publicKeyId identifier, unwrapKeyFn identifier) definition {
+func (module dictModule) singletonDictDef() definition {
 	return definition{
 		localName: "singleton",
 		source: []string{
-			"singleton : " + publicKeyId.fullName() + " -> v -> " + wrapperType.name + " v",
-			"singleton k v = " + wrapperType.name + " (Dict.singleton (" + unwrapKeyFn.fullName() + " k) v)",
+			"singleton : " + module.publicKeyType.fullName() + " -> v -> " + module.wrapperType.name + " v",
+			"singleton k v = " + module.wrapperType.name + " (Dict.singleton (" + module.unwrapKeyFn.fullName() + " k) v)",
 		},
 	}
 }
 
 func (module dictModule) source() []string {
 	definitions := []definition{
-		dictDef(module.wrapperType, module.privateKeyType),
-		emptyDictDef(module.wrapperType, module.publicKeyType),
-		singletonDictDef(module.wrapperType, module.publicKeyType, module.unwrapKeyFn),
+		module.dictDef(),
+		module.emptyDictDef(),
+		module.singletonDictDef(),
 	}
 
 	exports := []string{}
