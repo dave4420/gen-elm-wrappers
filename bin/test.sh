9d889c23844b56bd4e1c9006d29bb20eeebf17e3
Dave Hinton <dave@4420.uk>
2023-10-25 09:59:31 +0100

    first stab at test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 set -euo pipefail
 
-echo DAVE
+rm -rf test
+
+go build .
+
+mkdir test
+cd test
+
+cat >elm.json <<EOF
+{
+    "type": "application",
+    "source-directories": [
+        "src"
+    ],
+    "elm-version": "0.19.1"
+}
+EOF
+
+mkdir src
+
+cat >src/Main.elm <<EOF
+module Main exposing (main)
+import Data.ByDate
+main = Debug.todo "main"
+EOF
+
+../gen-elm-wrappers
+
+elm make src/Main.elm --debug
