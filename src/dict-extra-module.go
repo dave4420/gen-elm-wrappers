9a0ad5f221ec342de48bf755c4130b08fdd7432c
Dave Hinton <dave@4420.uk>
2023-10-29 11:28:20 +0000

    add defs for manipulation fns

--- src/dict-extra-module.go
+++ src/dict-extra-module.go
@@ -11,6 +11,9 @@ func (module dictModule) extraDefs() []definition {
 		module.fromListDedupeDef(),
 		module.fromListDedupeByDef(),
 		module.frequenciesDef(),
+		module.removeWhenDef(),
+		module.insertDedupeDef(),
+		module.filterMapDef(),
 	}
 }
 
@@ -75,3 +78,54 @@ func (module dictModule) frequenciesDef() definition {
 		},
 	}
 }
+
+// Manipulation
+
+func (module dictModule) removeWhenDef() definition {
+	return definition{
+		localName: "removeWhen",
+		source: []string{
+			"removeWhen : (" + module.publicKeyType.fullName() + " -> v -> Bool) -> " + module.wrapperType.name + " v -> " + module.wrapperType.name + " v",
+			"removeWhen f (" + module.wrapperType.name + " d) =",
+			"  let",
+			"    g k v = case " + module.wrapKeyFn.fullName() + " k of",
+			"      Nothing -> True",
+			"      Just kk -> f kk v",
+			"  in " + module.wrapperType.name + " (Dict.Extra.removeWhen g d)",
+		},
+	}
+}
+
+// DAVE: add removeMany when Set module generated
+// DAVE: add keepOnly when Set module generated
+
+func (module dictModule) insertDedupeDef() definition {
+	return definition{
+		localName: "insertDedupe",
+		source: []string{
+			"insertDedupe : (v -> v -> v) -> " + module.publicKeyType.fullName() + " -> v -> " + module.wrapperType.name + " v -> " + module.wrapperType.name + " v",
+			"insertDedupe f k v (" + module.wrapperType.name + " d) =",
+			"  " + module.wrapperType.name + " <| Dict.Extra.insertDedupe f (" + module.unwrapKeyFn.fullName() + " k) v d",
+		},
+	}
+}
+
+// DAVE: handle variations of mapKeys
+
+func (module dictModule) filterMapDef() definition {
+	return definition{
+		localName: "filterMap",
+		source: []string{
+			"filterMap : (" + module.publicKeyType.fullName() + " -> a -> Maybe b) -> " + module.wrapperType.name + " a -> " + module.wrapperType.name + " b",
+			"filterMap f (" + module.wrapperType.name + " d) =",
+			"  let",
+			"    g k v = case " + module.wrapKeyFn.fullName() + " k of",
+			"      Nothing -> Nothing",
+			"      Just kk -> f kk v",
+			"  in",
+			"  " + module.wrapperType.name + " <| Dict.Extra.filterMap g d",
+		},
+	}
+}
+
+// DAVE: handle variations of invert
