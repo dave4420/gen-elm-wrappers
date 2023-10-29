e7c3e910dbcf0a4e8927ad9852ffdedbc7c7fa0c
Dave Hinton <dave@4420.uk>
2023-10-29 10:11:49 +0000

    add defs for filterGroupByDef, fromListByDef

--- src/dict-extra-module.go
+++ src/dict-extra-module.go
@@ -6,6 +6,8 @@ func (module dictModule) extraDefs() []definition {
 	}
 	return []definition{
 		module.groupByDef(),
+		module.filterGroupByDef(),
+		module.fromListByDef(),
 	}
 }
 
@@ -17,6 +19,26 @@ func (module dictModule) groupByDef() definition {
 		source: []string{
 			"groupBy : (a -> " + module.publicKeyType.fullName() + ") -> List a -> " + module.wrapperType.name + " (List a)",
 			"groupBy f xs = " + module.wrapperType.name + " (Dict.Extra.groupBy (\\x -> " + module.unwrapKeyFn.fullName() + " (f x)) xs)",
+		},
+	}
+}
+
+func (module dictModule) filterGroupByDef() definition {
+	return definition{
+		localName: "filterGroupBy",
+		source: []string{
+			"filterGroupBy : (a -> Maybe " + module.publicKeyType.fullName() + ") -> List a -> " + module.wrapperType.name + " (List a)",
+			"filterGroupBy f xs = " + module.wrapperType.name + " (Dict.Extra.filterGroupBy (\\x -> Maybe.map " + module.unwrapKeyFn.fullName() + " (f x)) xs)",
+		},
+	}
+}
+
+func (module dictModule) fromListByDef() definition {
+	return definition{
+		localName: "fromListBy",
+		source: []string{
+			"fromListBy : (a -> " + module.publicKeyType.fullName() + ") -> List a -> " + module.wrapperType.name + " a",
+			"fromListBy f xs = " + module.wrapperType.name + " (Dict.Extra.fromListBy (\\x -> " + module.unwrapKeyFn.fullName() + " (f x)) xs)",
 		},
 	}
 }
