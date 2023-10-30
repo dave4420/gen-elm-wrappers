e0d8f7410ef7061b6c21e465fab69ec3ad982acb
Dave Hinton <dave@4420.uk>
2023-10-30 08:03:29 +0000

    add more tests around reading dict module config

--- src/read-config_test.go
+++ src/read-config_test.go
@@ -7,7 +7,8 @@ func TestDecodesDictModuleConfig(t *testing.T) {
 	input := `{
 		"dependencies": {
 			"direct": {
-				"elm/core": "1.0.5"
+				"elm/core": "1.0.5",
+				"elm-community/dict-extra": "2.4.0"
 			}
 		}
 	}`
@@ -32,7 +33,7 @@ func TestDecodesDictModuleConfig(t *testing.T) {
 			name:       "fromInt",
 		},
 		elmCoreVersion:   "1.0.5",
-		dictExtraVersion: "",
+		dictExtraVersion: "2.4.0",
 	}
 
 	// when
@@ -44,5 +45,50 @@ func TestDecodesDictModuleConfig(t *testing.T) {
 	}
 	if output.modules[0] != expectedModule {
 		t.Errorf("Unexpected output: %v", output)
+	}
+}
+
+func TestDecodesDictModuleConfigWithoutDictExtra(t *testing.T) {
+	// given
+	input := `{
+		"dependencies": {
+			"direct": {
+				"elm/core": "1.0.5"
+			}
+		}
+	}`
+
+	// when
+	output, err := decodeConfigFromBlob([]byte(input))
+
+	// then
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	module, ok := output.modules[0].(dictModule)
+	if !ok {
+		t.Errorf("Unexpected output: %v", output)
+	}
+	if module.dictExtraVersion != "" {
+		t.Errorf("Unexpected dict-extra version: %v", module.dictExtraVersion)
+	}
+}
+
+func TestDoesntDecodeDictModuleConfigWithoutElmCore(t *testing.T) {
+	// given
+	input := `{
+		"dependencies": {
+			"direct": {
+				"elm-community/dict-extra": "2.4.0"
+			}
+		}
+	}`
+
+	// when
+	output, err := decodeConfigFromBlob([]byte(input))
+
+	// then
+	if err == nil {
+		t.Errorf("Expected error, instead got output: %v", output)
 	}
 }
