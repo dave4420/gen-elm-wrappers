4dabaa3183a969083bee1a551c6f16c8250c8706
Dave Hinton <dave@4420.uk>
2023-11-20 10:08:21 +0000

    todos

--- src/main.go
+++ src/main.go
@@ -157,8 +157,7 @@ func main() {
 //       go:
 //        -  $GOOS and $GOARCH
 //        -  valid combinations listed at https://go.dev/doc/install/source#environment
-// DAVE: create a release containing the binaries for each arch
 // DAVE: get and save npm creds to 1password
 // DAVE: install 1password in github actions and demo it fetching text from 1password
 // DAVE: munge package.json to set version number and make it publishable
-// DAVE: actually publish the package
+// DAVE: actually publish the package to npm
