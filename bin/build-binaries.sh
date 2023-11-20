79e156479ceb0a54fe112d096e2cf2c6814aa30d
Dave Hinton <dave@4420.uk>
2023-11-20 10:03:40 +0000

    deduce binary file extension from os

--- bin/build-binaries.sh
+++ bin/build-binaries.sh
@@ -11,13 +11,21 @@ list-binaries-to-build() {
     echo darwin amd64
     echo darwin arm64
     echo linux amd64
-    echo windows amd64 .exe
+    echo windows amd64
 }
 
 rm -rf out
 mkdir out
 
-list-binaries-to-build | while read GOOS GOARCH BINARY_EXT ; do
+list-binaries-to-build | while read GOOS GOARCH ; do
+    case $GOOS in
+        windows)
+            BINARY_EXT=.exe
+            ;;
+        *)
+            BINARY_EXT=''
+            ;;
+    esac
     BINARY_NAME="out/gen-elm-wrappers-$GOOS-$GOARCH-$BINARY_VERSION$BINARY_EXT"
     export GOOS GOARCH BINARY_EXT BINARY_NAME
     bin/build-binary.sh
