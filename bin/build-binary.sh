c9f67cc6a62f155f2cd28c5e7ca976f114cebe0b
Dave Hinton <dave@4420.uk>
2023-11-20 09:20:59 +0000

    add BINARY_NAME env var to build script

--- bin/build-binary.sh
+++ bin/build-binary.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[ -z "$BINARY_NAME" ]] ; then
+    printf 'BINARY_NAME not set\n' >&2
+    exit 1
+fi
+
 if [[ -z "$BINARY_VERSION" ]] ; then
     printf 'BINARY_VERSION not set\n' >&2
     exit 1
 fi
 
 go build \
-    -o gen-elm-wrappers \
+    -o "$BINARY_NAME" \
     -ldflags "-X main.Version=$BINARY_VERSION" \
     github.com/dave4420/gen-elm-wrappers/src
