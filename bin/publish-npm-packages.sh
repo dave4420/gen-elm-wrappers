def77afdf0cb6cdb619a77201d0c2b070a7c9312
Dave Hinton <dave@4420.uk>
2024-03-30 15:48:34 +0000

    update publish-npm-packages.sh to search in subdirs

--- bin/publish-npm-packages.sh
+++ bin/publish-npm-packages.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 cd npm
 
-for package in $(ls | perl -e 'print sort { length($b) <=> length($a) } <>') ; do
+for package in $(find . -type d -exec sh -c '[ -f {}/package.json ]' \; -print) ; do
     # ordered so that the package with the shortest name is published last,
     # after its dependencies
     if ! npm publish $package ; then
