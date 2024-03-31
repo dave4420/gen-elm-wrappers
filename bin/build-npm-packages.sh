d73d9b27a383c9cfa3146b99954f7184f915c180
Dave Hinton <dave@4420.uk>
2024-03-31 08:48:17 +0100

    rm remains of gomplate stuff

--- bin/build-npm-packages.sh
+++ bin/build-npm-packages.sh
@@ -8,29 +8,6 @@ mkdir npm
 
 package_name=gen-elm-wrappers
 
-platform="$(node -e 'console.log(process.platform)')"
-case "$platform" in
-    linux)
-        ;;
-    darwin)
-        ;;
-    *)
-        printf 'Unsupported node process.platform: %s\n' "$platform" >&2
-        exit 2
-esac
-
-arch="$(node -e 'console.log(process.arch)')"
-case "$arch" in
-    x64)
-        arch=amd64
-        ;;
-    arm64)
-        ;;
-    *)
-        printf 'Unsupported node process.arch: %s\n' "$arch" >&2
-        exit 2
-esac
-
 # general approach taken from <https://sentry.engineering/blog/publishing-binaries-on-npm>
 
 list-binaries-to-build() {
