065a601b823a963ec7940b4b7b8ea285bff7784c
Dave Hinton <dave@4420.uk>
2024-04-20 15:05:00 +0100

    wrap build set defs

--- src/set-core-module.go
+++ src/set-core-module.go
@@ -10,6 +10,10 @@ func (module setModule) coreDefs(elmCoreVersion version) ([]definition, error) {
 	if elmCoreVersion.isSameMajorAndNotEarlierMinorThan(supportedElmCoreVersion) {
 		return []definition{
 			module.setDef(),
+			module.emptyDef(),
+			module.singletonDef(),
+			module.insertDef(),
+			module.removeDef(),
 		}, nil
 	}
 	return []definition{}, errors.New("Versions " + elmCoreVersion.toString() + " of elm/core " +
@@ -33,6 +37,48 @@ func (module setModule) setDef() definition {
 				},
 				" ",
 			),
+		},
+	}
+}
+
+// Build
+
+func (module setModule) emptyDef() definition {
+	return definition{
+		localName: "empty",
+		source: []string{
+			"empty : " + module.wrapperType.name,
+			"empty = " + module.wrapperType.name + " Set.empty",
+		},
+	}
+}
+
+func (module setModule) singletonDef() definition {
+	return definition{
+		localName: "singleton",
+		source: []string{
+			"singleton : " + module.publicKeyType.fullName() + " -> " + module.wrapperType.name,
+			"singleton k = " + module.wrapperType.name + " (Set.singleton (" + module.unwrapKeyFn.fullName() + " k))",
+		},
+	}
+}
+
+func (module setModule) insertDef() definition {
+	return definition{
+		localName: "insert",
+		source: []string{
+			"insert : " + module.publicKeyType.fullName() + " -> " + module.wrapperType.name + " -> " + module.wrapperType.name,
+			"insert k (" + module.wrapperType.name + " d) = " + module.wrapperType.name + " (Set.insert (" + module.unwrapKeyFn.fullName() + " k) d)",
+		},
+	}
+}
+
+func (module setModule) removeDef() definition {
+	return definition{
+		localName: "remove",
+		source: []string{
+			"remove : " + module.publicKeyType.fullName() + " -> " + module.wrapperType.name + " -> " + module.wrapperType.name,
+			"remove k (" + module.wrapperType.name + " d) = " + module.wrapperType.name + " (Set.remove (" + module.unwrapKeyFn.fullName() + " k) d)",
 		},
 	}
 }
