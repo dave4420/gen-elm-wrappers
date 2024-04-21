e8cdc8c544d06d148729e0e4402a0cfebd9d80ef
Dave Hinton <dave@4420.uk>
2024-04-21 10:04:55 +0100

    wrap set-extra defs

--- src/set-extra-module.go
+++ src/set-extra-module.go
@@ -9,6 +9,8 @@ func (module setModule) extraDefs(setExtraVersion *version) ([]definition, error
 	supportedSetExtraVersion := version{major: 1, minor: 2}
 	if setExtraVersion.isSameMajorAndNotEarlierMinorThan(supportedSetExtraVersion) {
 		return []definition{
+			module.subsetDef(),
+			module.toggleDef(),
 		}, nil
 	}
 	return []definition{}, errors.New("Versions " + setExtraVersion.toString() +
@@ -17,4 +19,27 @@ func (module setModule) extraDefs(setExtraVersion *version) ([]definition, error
 	)
 }
 
-// DAVE: add actual wrappers
+// DAVE: handle variations of concatMap
+// DAVE: handle variations of filterMap
+
+func (module setModule) subsetDef() definition {
+	return definition{
+		localName: "subset",
+		source: []string{
+			"subset : " + module.wrapperType.name + " -> " + module.wrapperType.name + " -> Bool",
+			"subset (" + module.wrapperType.name + " set1) (" + module.wrapperType.name + " set2) =",
+			"    Set.Extra.subset set1 set2",
+		},
+	}
+}
+
+func (module setModule) toggleDef() definition {
+	return definition{
+		localName: "toggle",
+		source: []string{
+			"toggle : " + module.privateKeyType.fullName() + " -> " + module.wrapperType.name + " -> " + module.wrapperType.name,
+			"toggle key (" + module.wrapperType.name + " set) =",
+			"    " + module.wrapperType.name + " <| Set.Extra.toggle key set",
+		},
+	}
+}
